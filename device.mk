48821743d87142cd8ce89c9a8da396b893aa004a
AdarshGrewal <adarshgrewal@gmail.com>
2026-05-02 22:38:13 +0300

    marble: Include MiuiCamera if exists Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
 
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-marble/device.mk)
+$(call soong_config_set,camera,package_name,com.android.camera)
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.marble.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.marble.rc
